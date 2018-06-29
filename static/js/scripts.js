9470d9ccef1f619e6db861bfc30dd6be3c6422ec
DustyShap <dustinshapiro@gmail.com>
2018-06-29 15:40:21 -0400

    more refactor

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
 
-  
+
   var participant_list = []
 
   $("#input_form").submit(function(event){
     event.preventDefault();
     $("#error_message").text("")
-    participant_list.push(cleanName($("#participant_name").val().trim()))
+    var $name = cleanName($("#participant_name").val().trim())
+    participant_list.push($name)
     participant_list = removeDupes(participant_list)
-    runningTotal(participant_list)
+    participantCounter(participant_list)
     $("#participant_name").val("")
     $("#generate_groups").removeAttr("disabled")
   })
@@ -20,63 +21,65 @@ $(document).ready(function(){
   })
 
 
-  function cleanName(name){
-    return name.toLowerCase().substr(0,1).toUpperCase()+name.substr(1)
-  }
+//Functions
+
+function cleanName(name){
+  return name.toLowerCase().substr(0,1).toUpperCase()+name.substr(1)
+}
 
 
-  function removeDupes(list) {
-    let unique = {};
-    list.forEach(function(i) {
-      if(!unique[i]) {
-        unique[i] = true;
-      }
-    });
-    return Object.keys(unique);
-  }
-
-
-  function runningTotal(list){
-    var total = list.length;
-    $("#total_participants").html("Entered Names: " + total);
-    if($("#"+total).length == 0){
-      $("#number_per_group").append("<option id=" + total +
-        " value="+ total + ">" + total + "</option>");
-    } else {
-      $("#error_message").text("Duplicate!")
-      setTimeout(function(){$("#error_message").text("")}, 1000)
+function removeDupes(list) {
+  let unique = {};
+  list.forEach(function(i) {
+    if(!unique[i]) {
+      unique[i] = true;
     }
+  });
+  return Object.keys(unique);
+}
+
+
+function participantCounter(list){
+  var total = list.length;
+  $("#total_participants").html("Entered Names: " + total);
+  if($("#"+total).length == 0){
+    $("#number_per_group").append("<option id=" + total +
+      " value="+ total + ">" + total + "</option>");
+  } else {
+    $("#error_message").text("Duplicate!")
+    setTimeout(function(){$("#error_message").text("")}, 1000)
   }
+}
 
 
-  function submitList(list,number){
-      $("#results").empty()
-      $.ajax({
-        data:{
-          list_of_names: list,
-          number_per_group: number
-        },
-        type:"POST",
-        url:"/generate_groups"
-      })
-      .done(function(data){
-        $("#generate_groups").text('Regenerate Groups')
-        displayGroups(data)
-      })
-  }
+function submitList(list,number){
+    $("#results").empty()
+    $.ajax({
+      data:{
+        list_of_names: list,
+        number_per_group: number
+      },
+      type:"POST",
+      url:"/generate_groups"
+    })
+    .done(function(data){
+      $("#generate_groups").text('Regenerate Groups')
+      displayGroups(data)
+    })
+}
 
 
-  function displayGroups(data){
-    for (var i=0; i < data.length; i++){
-      var group_count = i+1
-      var groups = data[i];
-      var $result = $("<div class='result'><b>Group " + group_count + ":</b></div>");
-      for (var j=0; j < groups.length; j++){
-        $result.append("<span>"+groups[j]+"</span>")
-      }
-      $("#results").append($result)
+function displayGroups(data){
+  for (var i=0; i < data.length; i++){
+    var group_count = i+1
+    var groups = data[i];
+    var $result = $("<div class='result'><b>Group " + group_count + ":</b></div>");
+    for (var j=0; j < groups.length; j++){
+      $result.append("<span>"+groups[j]+"</span>")
     }
+    $("#results").append($result)
   }
+}
 
 
 })
--- tests.py
+++ tests.py
@@ -1,6 +1,9 @@
 import unittest
 from app import app, generate_groups, process_list
 
+test_list = ['John', 'Jill', 'James', 'Jessica', 'Jackie']
+test_list2 = ['Bob', 'Bill', 'Beth', 'Brittney', 'Becky']
+
 
 class BasicTests(unittest.TestCase):
     def test_main_page(self):
@@ -8,11 +11,16 @@ class BasicTests(unittest.TestCase):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
 
-    def test_process_list(self):
-        test_list = ['John', 'Jill', 'James', 'Jessica', 'Emily']
-        test_list2 = ['Bob', 'Bill', 'Beth', 'Brittney', 'Becky']
+    def test_groups_exist(self):
+        self.assertTrue(process_list(test_list, 2))
+
+    def test_process_list_equal(self):
         self.assertEqual(len(process_list(test_list, 2)),
-                         len(process_list(test_list, 2)))
+                         len(process_list(test_list2, 2)))
+
+    def test_process_list_not_equal(self):
+        self.assertNotEqual(len(process_list(test_list, 2)),
+                            len(process_list(test_list2, 3)))
 
 
 if __name__ == '__main__':
