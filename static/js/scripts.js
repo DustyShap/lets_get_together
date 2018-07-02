f38484135735abdfa9ffa65debc08e9ee30ab3c3
DustyShap <dustinshapiro@gmail.com>
2018-07-02 13:21:40 -0400

    delete user function

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -21,65 +21,77 @@ $(document).ready(function(){
   })
 
 
-//Functions
-
-function cleanName(name){
-  return name.substr(0,1).toUpperCase()+name.substr(1)
-}
-
-
-function removeDupes(list) {
-  let unique = {};
-  list.forEach(function(i) {
-    if(!unique[i]) {
-      unique[i] = true;
+  $("#delete_user").click(function(event){
+    event.preventDefault();
+    console.log(participant_list)
+    var $name_to_delete = $("#user_to_delete").val().trim().toLowerCase()
+    var index = participant_list.indexOf(cleanName($name_to_delete))
+    if (index !== -1){
+      participant_list.splice(index,1);
+      submitList(JSON.stringify(participant_list), $("#number_per_group").val())
+    } else {
+      alert("Name doesn't exist")
     }
-  });
-  return Object.keys(unique);
-}
+  })
+  //Functions
 
-
-function participantCounter(list){
-  var total = list.length;
-  $("#total_participants").html("Entered Names: " + total);
-  if($("#"+total).length == 0){
-    $("#number_per_group").append("<option id=" + total +
-      " value="+ total + ">" + total + "</option>");
-  } else {
-    $("#error_message").text("Duplicate!")
-    setTimeout(function(){$("#error_message").text("")}, 1000)
+  function cleanName(name){
+    return name.substr(0,1).toUpperCase()+name.substr(1)
   }
-}
 
 
-function submitList(list,number){
-    $("#results").empty()
-    $.ajax({
-      data:{
-        list_of_names: list,
-        number_per_group: number
-      },
-      type:"POST",
-      url:"/generate_groups"
-    })
-    .done(function(data){
-      $("#generate_groups").text('Regenerate Groups')
-      displayGroups(data)
-    })
-}
+  function removeDupes(list) {
+    let unique = {};
+    list.forEach(function(i) {
+      if(!unique[i]) {
+        unique[i] = true;
+      }
+    });
+    return Object.keys(unique);
+  }
 
 
-function displayGroups(data){
-  for (var i=0; i < data.length; i++){
-    var group_count = i+1
-    var groups = data[i];
-    var $result = $("<div class='result'><b>Group " + group_count + ":</b></div>");
-    for (var j=0; j < groups.length; j++){
-      $result.append("<span>"+groups[j]+"</span>")
+  function participantCounter(list){
+    var total = list.length;
+    $("#total_participants").html("Entered Names: " + total);
+    if($("#"+total).length == 0){
+      $("#number_per_group").append("<option id=" + total +
+        " value="+ total + ">" + total + "</option>");
+    } else {
+      $("#error_message").text("Duplicate!")
+      setTimeout(function(){$("#error_message").text("")}, 1000)
     }
-    $("#results").append($result)
   }
-}
+
+
+  function submitList(list,number){
+      $("#results").empty()
+      $.ajax({
+        data:{
+          list_of_names: list,
+          number_per_group: number
+        },
+        type:"POST",
+        url:"/generate_groups"
+      })
+      .done(function(data){
+        $("#generate_groups").text('Regenerate Groups')
+        displayGroups(data)
+      })
+  }
+
+
+  function displayGroups(data){
+    for (var i=0; i < data.length; i++){
+      var group_count = i+1
+      var groups = data[i];
+      var $result = $("<div class='result'><b>Group " + group_count + ":</b></div>");
+      for (var j=0; j < groups.length; j++){
+        $result.append("<span>"+groups[j]+"</span>")
+      }
+      $("#results").append($result)
+    }
+  }
 
 
 })
