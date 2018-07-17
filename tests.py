e1657653cd203227c7564b4fa65083c79964fcb9
DustyShap <dustinshapiro@gmail.com>
2018-07-17 12:21:11 -0500

    file tests

--- tests.py
+++ tests.py
@@ -1,13 +1,13 @@
-import os
 import unittest
+import os
 from app import app, generate_groups, process_list
-
-THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-MY_DATA_PATH = os.path.join(THIS_DIR, 'names.txt')
-
+from file_input import file_to_list
 
 test_list = ['John', 'Jill', 'James', 'Jessica', 'Jackie']
 test_list2 = ['Bob', 'Bill', 'Beth', 'Brittney', 'Becky']
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+MY_DATA_PATH = os.path.join(THIS_DIR, 'names.txt')
 
 class BasicTests(unittest.TestCase):
     def test_main_page(self):
@@ -29,21 +29,16 @@ class BasicTests(unittest.TestCase):
 
 class FileTests(unittest.TestCase):
     def setUp(self):
-        self.test_data = open(MY_DATA_PATH).read().splitlines()
+        self.testdata = open(MY_DATA_PATH)
 
-    def test_is_data_a_list(self):
-        self.assertTrue(isinstance(self.test_data,list))
+    def test_does_file_exist(self):
+        self.assertTrue(self.testdata)
 
-    def test_if_file_exists(self):
-        self.assertTrue(self.test_data)
+    def test_file_to_list(self):
+        self.assertTrue(isinstance(file_to_list(MY_DATA_PATH),list))
 
     def test_process_file(self):
-        self.assertNotEqual(len(process_list(self.test_data,2)),
-                         len(process_list(self.test_data,3)))
-
-    def test_group_numbers(self):
-        pass
-
+        self.assertTrue(process_list(file_to_list(MY_DATA_PATH),3))
 
 
 if __name__ == '__main__':
