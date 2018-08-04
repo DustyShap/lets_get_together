9b3d28b9537c6fbba415c5b30d32f59d849f73f5
DustyShap <dustinshapiro@gmail.com>
2018-08-03 19:16:27 -0500

    commit

--- group.py
+++ group.py
@@ -6,15 +6,10 @@ class Group():
         return file.strip()
 
     def load_names(self,file):
-        while True:
-            try:
-                name_list = records = open(file,"r").read().splitlines()
-                return name_list
-            except:
-                print('Invalid')
-                pass
-            else:
-                break
+        name_list = records = open(file,"r").read().splitlines()
+        return name_list
+
+
 
     def process_list(self,participant_list, group_size):
         groups = []
@@ -29,7 +24,13 @@ if __name__ == '__main__':
     print('Welcome to the Lets Get Together Group Generator')
     print('------------------------------------------------')
     group = Group()
-    filename = group.sanitize_filename(input("What is the name of the input file? "))
-    group_list = group.load_names(filename)
-    group_numbers = int(input("How many people per group? "))
-    print(group.process_list(group_list,group_numbers))
+    while True:
+        try:
+            filename = group.sanitize_filename(input("What is the name of the input file? "))
+            group_list = group.load_names(filename)
+            group_numbers = int(input("How many people per group? "))
+            print(group.process_list(group_list,group_numbers))
+            break
+        except:
+            print('Invalid')
+            break
